40438355075f534e84f3d617674ce4e76efbc0ea
emirhancakirr <ecakir20@ku.edu.tr>
2026-01-17 15:38:47 +0300

    configuration propoerties are added and enabled

--- src/main/java/com/smartpace/smartpace/SmartpaceApplication.java
+++ src/main/java/com/smartpace/smartpace/SmartpaceApplication.java
@@ -2,8 +2,12 @@ package com.smartpace.smartpace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.smartpace.smartpace.config.Concept2ApiProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(Concept2ApiProperties.class)
 public class SmartpaceApplication {
 
 	public static void main(String[] args) {
