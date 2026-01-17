a230bc24459d78595f84cd63ca68882c34100aba
emirhancakirr <ecakir20@ku.edu.tr>
2026-01-17 15:33:00 +0300

    webClientCongif added

--- src/main/java/com/smartpace/smartpace/config/WebClientConfig.java
+++ src/main/java/com/smartpace/smartpace/config/WebClientConfig.java
@@ -1,5 +1,31 @@
 package com.smartpace.smartpace.config;
 
-public class WebClientConfig {
+import java.time.Duration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class WebClientConfig {
+    
+    private final Concept2ApiProperties properties;
+    
+    public WebClientConfig(Concept2ApiProperties properties) {
+        this.properties = properties;
+    }
+    
+    @Bean
+    public WebClient concept2WebClient() {
+        HttpClient httpClient = HttpClient.create()
+            .responseTimeout(Duration.ofMillis(properties.getTimeout()));
+        
+        return WebClient.builder()
+            .baseUrl(properties.getBaseUrl())
+            .clientConnector(new ReactorClientHttpConnector(httpClient))
+            .build();
+    }
 }
