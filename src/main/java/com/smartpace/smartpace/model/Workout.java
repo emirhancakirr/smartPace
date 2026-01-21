86de06b38905c776681067c974ec94635855253b
emirhancakirr <ecakir20@ku.edu.tr>
2026-01-21 23:57:33 +0300

    workout model is implemented

--- src/main/java/com/smartpace/smartpace/model/Workout.java
+++ src/main/java/com/smartpace/smartpace/model/Workout.java
@@ -1,5 +1,42 @@
 package com.smartpace.smartpace.model;
 
-public class Workout {
+import lombok.Builder;
+import lombok.Value;
 
+import java.time.LocalDateTime;
+
+/**
+ * Domain model representing a rowing workout.
+ * Immutable value object containing workout metrics.
+ */
+@Value
+@Builder
+public class Workout {
+    
+    /**
+     * Date and time when the workout was performed
+     */
+    LocalDateTime date;
+    
+    /**
+     * Total distance covered in meters
+     */
+    Integer distance;
+    
+    /**
+     * Average split time per 500m in deciseconds.
+     * Example: 1052 = 1 minute 45.2 seconds per 500m
+     */
+    Integer split;
+    
+    /**
+     * Average power output in watts
+     */
+    Integer watts;
+    
+    /**
+     * Average heart rate in beats per minute (BPM).
+     * Optional - may be null if heart rate monitor was not used
+     */
+    Integer heartRate;
 }
