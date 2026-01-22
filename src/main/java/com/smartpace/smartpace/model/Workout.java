29448432daded89c6cc9b78e00d1160872f85865
emirhancakirr <ecakir20@ku.edu.tr>
2026-01-22 23:56:14 +0300

    the location of workoutTest changed

--- src/main/java/com/smartpace/smartpace/model/Workout.java
+++ src/main/java/com/smartpace/smartpace/model/Workout.java
@@ -5,38 +5,15 @@ import lombok.Value;
 
 import java.time.LocalDateTime;
 
-/**
- * Domain model representing a rowing workout.
- * Immutable value object containing workout metrics.
- */
 @Value
 @Builder
 public class Workout {
     
-    /**
-     * Date and time when the workout was performed
-     */
+
     LocalDateTime date;
-    
-    /**
-     * Total distance covered in meters
-     */
     Integer distance;
-    
-    /**
-     * Average split time per 500m in deciseconds.
-     * Example: 1052 = 1 minute 45.2 seconds per 500m
-     */
+    //it is defined in the minute per 500m
     Integer split;
-    
-    /**
-     * Average power output in watts
-     */
     Integer watts;
-    
-    /**
-     * Average heart rate in beats per minute (BPM).
-     * Optional - may be null if heart rate monitor was not used
-     */
     Integer heartRate;
 }
