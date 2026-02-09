c453c4275ab9f9c82f3d9a3cbfd06a31c996d209
emirhancakirr <ecakir20@ku.edu.tr>
2026-02-09 16:35:18 +0300

    logging added

--- src/main/java/com/smartpace/smartpace/config/WebClientConfig.java
+++ src/main/java/com/smartpace/smartpace/config/WebClientConfig.java
@@ -2,11 +2,15 @@ package com.smartpace.smartpace.config;
 
 import java.time.Duration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 @Configuration
@@ -24,7 +28,23 @@ public class WebClientConfig {
             .responseTimeout(Duration.ofMillis(properties.getTimeout()));
         
         return WebClient.builder()
+            .filter(logRequestResponse())
             .clientConnector(new ReactorClientHttpConnector(httpClient))
             .build();
+    }
+
+    private static ExchangeFilterFunction logRequestResponse() {
+        Logger log = LoggerFactory.getLogger(WebClientConfig.class);
+        return ExchangeFilterFunction.ofRequestProcessor(request -> {
+            if (log.isDebugEnabled()) {
+                log.debug("Concept2 API request: {} {}", request.method(), request.url());
+            }
+            return Mono.just(request);
+        }).andThen(ExchangeFilterFunction.ofResponseProcessor(response -> {
+            if (log.isDebugEnabled()) {
+                log.debug("Concept2 API response: status={}", response.statusCode());
+            }
+            return Mono.just(response);
+        }));
     }
 }
