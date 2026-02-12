38645f9d47f87dfa7b936f9a40cb6d44d2e05531
emirhancakirr <ecakir20@ku.edu.tr>
2026-02-12 23:52:57 +0300

    exceptions are added

--- src/main/java/com/smartpace/smartpace/exception/Concept2ApiException.java
+++ src/main/java/com/smartpace/smartpace/exception/Concept2ApiException.java
@@ -2,13 +2,21 @@ package com.smartpace.smartpace.exception;
 
 public class Concept2ApiException extends RuntimeException{
 
-    public Concept2ApiException(String message){
+    private final int statustCode;
+
+    public Concept2ApiException(String message, int statusCode){
         super(message);
+        this.statustCode = statusCode;
 
     }
 
 
-    public Concept2ApiException(String message, Throwable cause){
+    public Concept2ApiException(String message, Throwable cause, int statusCode){
         super(message,cause);
+        this.statustCode = statusCode;
+    }
+
+    public int getStatustCode() {
+        return statustCode;
     }
 }
